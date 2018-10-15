e50e60fd37da419a6518194842f61a5b79d4e577
Amir Off <AMEER157@GMAIL.COM>
2018-10-15 18:51:38 +0300

    [skip ci]

--- backend/model/index.js
+++ backend/model/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Connecting to MongoDB
 // "C:\mongodb\bin\mongod.exe" --dbpath="C:\mongodb\data"
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useCreateIndex: true}, (err) => {
+mongoose.connect(process.env.MONGODB_URI, (err) => {
   if (!err) {
     console.log('MongoDB connection succeeded.');
   }
