fe08fbbdef8e1f51df70e15bf3fe98805bf56b36
amiroffme <amir@zenprotocol.com>
2020-03-30 00:21:14 +0300

    expand JWT expiration period

--- backend/config/index.js
+++ backend/config/index.js
@@ -5,13 +5,13 @@ const config = {
     "PORT": 80,
     "MONGODB_URI": "mongodb://127.0.0.1:27017/smarthaus",
     "JWT_SECRET": "SECRET#123",
-    "JWT_EXP": "2h"
+    "JWT_EXP": "24h"
   },
   "production": {
     "PORT": process.env.PORT,
     "MONGODB_URI": "mongodb://xxxxx/DB_Name",
     "JWT_SECRET": "SECRET#123",
-    "JWT_EXP": "2h"
+    "JWT_EXP": "24h"
   }
 };
 
